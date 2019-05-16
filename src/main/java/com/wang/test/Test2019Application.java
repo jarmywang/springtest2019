10167054b49d361819535b97109c3399a2426407
汪建军 <wjj@dianmi365.com>
2019-05-16 19:36:26 +0800

    init

---
+++ src/main/java/com/wang/test/Test2019Application.java
@@ -0,0 +1,13 @@
+package com.wang.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2019Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test2019Application.class, args);
+    }
+
+}

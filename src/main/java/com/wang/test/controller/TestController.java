0098a07e05dadec6fe68e146f24fbbc71a08f98e
汪建军 <wjj@dianmi365.com>
2019-05-21 15:36:45 +0800

    init

--- src/main/java/com/wang/test/controller/TestController.java
+++ src/main/java/com/wang/test/controller/TestController.java
@@ -4,13 +4,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping(produces = "application/json;charset=UTF-8", method = RequestMethod.POST)
 public class TestController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String hello() {
-        return "hello spring!!\nSystem.currentTimeMillis()= " + System.currentTimeMillis() + "\nby:" + getClass().getName();
+        Map<Long, String> users =  Collections.synchronizedMap(new HashMap<>());
+        return "hello pring!!\nSystem.currentTimeMillis()= " + System.currentTimeMillis() + "\nby:" + getClass().getName();
     }
 
 }
